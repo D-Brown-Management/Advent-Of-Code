f7005804ddfe1c051ac5ec4313b68776de900514
Steve Jost <steve@stevejost.com>
2015-12-06 05:04:20 -0800

    F# cleanup for checkin.

--- AdventDay2FSharp/Program.fs
+++ AdventDay2FSharp/Program.fs
@@ -1,6 +1,4 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
-let getPaper (fileLines:seq<string>) =
+﻿let getPaper (fileLines:seq<string>) =
     fileLines
     |> Seq.map ( fun s -> s.Split('x') |> Array.map (fun x -> x |> int))
     |> Seq.toList      
@@ -18,12 +16,11 @@ let getRibbon (fileLines:seq<string>) =
 
 [<EntryPoint>]
 let main argv = 
-    let fileArray = System.IO.File.ReadLines(@"c:\users\steve\desktop\input5.txt")
+    let fileArray = System.IO.File.ReadLines("input.txt")
     let wallpaperNeeded = getPaper fileArray
     let ribbonNeeded = getRibbon fileArray
-//    let ary = [ [4;23;21];[22;29;19];[11;4;11];[8;10;5];[24;18;16] ]
-//
-//    let output =
-//        test
-//        
-    0 // return an integer exit code
+    printfn "The elves need %i wallpaper." wallpaperNeeded
+    printfn "The elves need %i ribbon." ribbonNeeded
+    printfn "Press any key to continue..."
+    System.Console.ReadLine()
+    0
